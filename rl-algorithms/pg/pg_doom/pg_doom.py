1d5035a707f82a9f4590cd89a36c5a0af3f3822b
Milan Nikolic <m.n.milan@hotmail.com>
2021-02-12 13:55:44 +0100

    Cleanup.

--- rl-algorithms/pg/pg_doom/pg_doom.py
+++ rl-algorithms/pg/pg_doom/pg_doom.py
@@ -1,14 +1,15 @@
-import tensorflow as tf         
-import numpy as np              
-from vizdoom import *           
-import random                   
-import time                     
-from skimage import transform   
+import tensorflow as tf
+import numpy as np
+from vizdoom import *
+import random
+import time
+from skimage import transform
 
 from collections import deque
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-import warnings 
+import warnings
+
 warnings.filterwarnings("ignore")
 
 from pgnet import PGNetwork
@@ -24,13 +25,14 @@ BATCH_SIZE = 2500
 GAMMA = 0.95
 TRAINING = True
 
+
 def create_env():
     """
-        Creates doom health gathering env.
-        params: none
-        returs: env, all possible actions
+    Creates doom health gathering env.
+    params: none
+    returs: env, all possible actions
     """
-    game = DoomGame()   
+    game = DoomGame()
     game.load_config("health_gathering.cfg")
     game.set_doom_scenario_path("health_gathering.wad")
     game.init()
@@ -40,33 +42,37 @@ def create_env():
 
 def preprocess_frame(frame):
     """
-        Preprocesses the input frame using IMGH and IMGW
-        Image is first cropped removing the useless information
-        then normalised and resized in the end
-        params: image
-        returns: preprocessed image
+    Preprocesses the input frame using IMGH and IMGW
+    Image is first cropped removing the useless information
+    then normalised and resized in the end
+    params: image
+    returns: preprocessed image
     """
-    
-    cropped_frame = frame[80:, :] # crop [UP:DOWN, LEFT:RIGHT]
+
+    cropped_frame = frame[80:, :]  # crop [UP:DOWN, LEFT:RIGHT]
     normalized_frame = cropped_frame / 255.0
 
     return transform.resize(normalized_frame, [IMGH, IMGW])
 
+
 def stack_frames(stacked_frames, state, is_new_episode):
     """
-        Creates a deque of stacked frames and a stacked_state
-        to be used as input to the neural net
-        params: deque of stacked frakes
-                state (frame)
-                indicator that tells if this state represents the start of the new episode
-        returns: updated frame stack
-                 stacked state to be used as input of the neural net 
+    Creates a deque of stacked frames and a stacked_state
+    to be used as input to the neural net
+    params: deque of stacked frakes
+            state (frame)
+            indicator that tells if this state represents the start of the new episode
+    returns: updated frame stack
+             stacked state to be used as input of the neural net
     """
-    
+
     frame = preprocess_frame(state)
     if is_new_episode:
         # reset the stack
-        stacked_frames = deque([np.zeros((IMGH, IMGW), dtype=np.int) for _ in range(STACK_SIZE)], maxlen=STACK_SIZE)
+        stacked_frames = deque(
+            [np.zeros((IMGH, IMGW), dtype=np.int) for _ in range(STACK_SIZE)],
+            maxlen=STACK_SIZE,
+        )
 
         # stack the same frame STACK_SIZE times
         for _ in range(STACK_SIZE):
@@ -84,14 +90,14 @@ def stack_frames(stacked_frames, state, is_new_episode):
 
 def discount_and_normalize_rewards(episode_rewards, gamma=GAMMA):
     """
-        Discounts the episode reward using some strange transformation ---
-        params: numpy array of rewards the agent has received in this episode
-                discount factor
-        returns: numpy array of discounted rewards
+    Discounts the episode reward using some strange transformation ---
+    params: numpy array of rewards the agent has received in this episode
+            discount factor
+    returns: numpy array of discounted rewards
     """
 
     discounted_episode_rewards = np.zeros_like(episode_rewards)
-    cumulative = 0.0  
+    cumulative = 0.0
     for i in reversed(range(len(episode_rewards))):
         cumulative = cumulative * gamma + episode_rewards[i]
         discounted_episode_rewards[i] = cumulative
@@ -103,22 +109,31 @@ def discount_and_normalize_rewards(episode_rewards, gamma=GAMMA):
 
 
 if __name__ == "__main__":
-    stacked_frames = deque([np.zeros((IMGH, IMGW), dtype=np.int) for _ in range(STACK_SIZE)], maxlen=STACK_SIZE)
+    stacked_frames = deque(
+        [np.zeros((IMGH, IMGW), dtype=np.int) for _ in range(STACK_SIZE)],
+        maxlen=STACK_SIZE,
+    )
     game, possible_actions = create_env()
-   
+
     tf.reset_default_graph()
 
     network = PGNetwork(STATE_SIZE, ACTION_SIZE, LEARNING_RATE)
 
     writer = tf.summary.FileWriter("./tensorboard/pg/test")
     tf.summary.scalar("Loss", network.loss)
-    tf.summary.scalar("Reward_mean", network.mean_reward_ )
+    tf.summary.scalar("Reward_mean", network.mean_reward_)
     write_op = tf.summary.merge_all()
 
     def make_batch(batch_size):
         global stacked_frames
 
-        states, actions, rewards_of_episode, rewards_of_batch, discounted_rewards = [], [], [], [], []
+        states, actions, rewards_of_episode, rewards_of_batch, discounted_rewards = (
+            [],
+            [],
+            [],
+            [],
+            [],
+        )
 
         episode_num = 1
         game.new_episode()
@@ -128,11 +143,15 @@ if __name__ == "__main__":
         state, stacked_frames = stack_frames(stacked_frames, frame, True)
 
         while True:
-            action_probability_distribution = sess.run(network.action_distribution,\
-                                                       feed_dict={network.inputs_:state.reshape(1, *STATE_SIZE)})
-        
-            action = np.random.choice(range(action_probability_distribution.shape[1]),\
-                                            p=action_probability_distribution[0])
+            action_probability_distribution = sess.run(
+                network.action_distribution,
+                feed_dict={network.inputs_: state.reshape(1, *STATE_SIZE)},
+            )
+
+            action = np.random.choice(
+                range(action_probability_distribution.shape[1]),
+                p=action_probability_distribution[0],
+            )
 
             action = possible_actions[action]
             reward = game.make_action(action)
@@ -144,10 +163,14 @@ if __name__ == "__main__":
 
             if done:
                 next_frame = np.zeros((IMGH, IMGW), dtype=int)
-                next_state, stacked_frames = stack_frames(stacked_frames, next_frame, False)
+                next_state, stacked_frames = stack_frames(
+                    stacked_frames, next_frame, False
+                )
 
                 rewards_of_batch.append(rewards_of_episode)
-                discounted_rewards.append(discount_and_normalize_rewards(rewards_of_episode))
+                discounted_rewards.append(
+                    discount_and_normalize_rewards(rewards_of_episode)
+                )
 
                 if len(np.concatenate(rewards_of_batch)) > batch_size:
                     break
@@ -155,21 +178,24 @@ if __name__ == "__main__":
                 rewards_of_episode = []
                 episode_num += 1
                 game.new_episode()
-                
+
                 frame = game.get_state().screen_buffer
                 state, stacked_frames = stack_frames(stacked_frames, frame, True)
 
             else:
                 next_frame = game.get_state().screen_buffer
-                next_state, stacked_frames = stack_frames(stacked_frames, next_frame, False)
+                next_state, stacked_frames = stack_frames(
+                    stacked_frames, next_frame, False
+                )
                 state = next_state
 
-
-        return np.stack(np.array(states)),\
-               np.stack(np.array(actions)),\
-               np.concatenate(rewards_of_batch),\
-               np.concatenate(discounted_rewards),\
-               episode_num 
+        return (
+            np.stack(np.array(states)),
+            np.stack(np.array(actions)),
+            np.concatenate(rewards_of_batch),
+            np.concatenate(discounted_rewards),
+            episode_num,
+        )
 
     all_rewards = []
     total_rewards = 0
@@ -188,49 +214,78 @@ if __name__ == "__main__":
             if TRAINING:
                 init = tf.global_variables_initializer()
                 sess.run(init)
-                
+
                 # Load the model
                 saver.restore(sess, "./models/model.ckpt")
                 print("Model restored.")
 
                 while epoch < NUM_EPOCHS + 1:
                     # Gather data
-                    states_mb, actions_mb, rewards_of_batch, discounted_rewards_mb, nb_episodes_mb =\
-                        make_batch(BATCH_SIZE)
+                    (
+                        states_mb,
+                        actions_mb,
+                        rewards_of_batch,
+                        discounted_rewards_mb,
+                        nb_episodes_mb,
+                    ) = make_batch(BATCH_SIZE)
 
                     total_reward_of_that_batch = np.sum(rewards_of_batch)
                     all_rewards.append(total_reward_of_that_batch)
 
-                    mean_reward_of_that_batch = np.divide(total_reward_of_that_batch, nb_episodes_mb)
+                    mean_reward_of_that_batch = np.divide(
+                        total_reward_of_that_batch, nb_episodes_mb
+                    )
                     mean_reward_total.append(mean_reward_of_that_batch)
 
-                    average_reward_of_all_training = np.divide(np.sum(mean_reward_total), epoch)
+                    average_reward_of_all_training = np.divide(
+                        np.sum(mean_reward_total), epoch
+                    )
                     maximum_reward_recorded = np.amax(all_rewards)
-
 
                     print("==========================================")
                     print("Epoch: ", epoch, "/", NUM_EPOCHS)
                     print("-----------")
                     print("Number of training episodes: {}".format(nb_episodes_mb))
                     print("Total reward: {}".format(total_reward_of_that_batch))
-                    print("Mean Reward of that batch {}".format(mean_reward_of_that_batch))
-                    print("Average Reward of all training: {}".format(average_reward_of_all_training))
-                    print("Max reward for a batch so far: {}".format(maximum_reward_recorded))
+                    print(
+                        "Mean Reward of that batch {}".format(mean_reward_of_that_batch)
+                    )
+                    print(
+                        "Average Reward of all training: {}".format(
+                            average_reward_of_all_training
+                        )
+                    )
+                    print(
+                        "Max reward for a batch so far: {}".format(
+                            maximum_reward_recorded
+                        )
+                    )
 
-                    loss_, _ = sess.run([network.loss, network.train_opt],
-                                        feed_dict={network.inputs_: states_mb.reshape((len(states_mb), IMGH ,IMGW, STACK_SIZE)),\
-                                                    network.actions: actions_mb,\
-                                                    network.discounted_episode_rewards_: discounted_rewards_mb })
-                                                                                
+                    loss_, _ = sess.run(
+                        [network.loss, network.train_opt],
+                        feed_dict={
+                            network.inputs_: states_mb.reshape(
+                                (len(states_mb), IMGH, IMGW, STACK_SIZE)
+                            ),
+                            network.actions: actions_mb,
+                            network.discounted_episode_rewards_: discounted_rewards_mb,
+                        },
+                    )
 
                     print("Training Loss: {}".format(loss_))
 
-                    summary = sess.run(write_op,\
-                                        feed_dict={network.inputs_: states_mb.reshape((len(states_mb), IMGH, IMGW, STACK_SIZE)),\
-                                                    network.actions: actions_mb,\
-                                                    network.discounted_episode_rewards_: discounted_rewards_mb, \
-                                                    network.mean_reward_: mean_reward_of_that_batch })
-                    
+                    summary = sess.run(
+                        write_op,
+                        feed_dict={
+                            network.inputs_: states_mb.reshape(
+                                (len(states_mb), IMGH, IMGW, STACK_SIZE)
+                            ),
+                            network.actions: actions_mb,
+                            network.discounted_episode_rewards_: discounted_rewards_mb,
+                            network.mean_reward_: mean_reward_of_that_batch,
+                        },
+                    )
+
                     writer.add_summary(summary, epoch)
                     writer.flush()
 
@@ -239,7 +294,6 @@ if __name__ == "__main__":
                         print("Model saved")
                     epoch += 1
             else:
-                
                 saver.restore(sess, "./models/model.ckpt")
 
                 for i in range(10):
@@ -248,25 +302,29 @@ if __name__ == "__main__":
                     state, stacked_frames = stack_frames(stacked_frames, frame, True)
 
                     while not game.is_episode_finished():
-                    
-                        action_probability_distribution = sess.run(network.action_distribution, \
-                                                                    feed_dict={network.inputs_: state.reshape(1, *STATE_SIZE)})
 
+                        action_probability_distribution = sess.run(
+                            network.action_distribution,
+                            feed_dict={network.inputs_: state.reshape(1, *STATE_SIZE)},
+                        )
 
-                        action = np.random.choice(range(action_probability_distribution.shape[1]),\
-                                                        p=action_probability_distribution[0]) 
-                                                 
+                        action = np.random.choice(
+                            range(action_probability_distribution.shape[1]),
+                            p=action_probability_distribution[0],
+                        )
+
                         action = possible_actions[action]
                         reward = game.make_action(action)
                         done = game.is_episode_finished()
-                        
+
                         if done:
                             break
                         else:
                             next_frame = game.get_state().screen_buffer
-                            next_state, stacked_frames = stack_frames(stacked_frames, next_frame, False)
+                            next_state, stacked_frames = stack_frames(
+                                stacked_frames, next_frame, False
+                            )
                             state = next_state
-        
 
                     print("Score for episode ", i, " :", game.get_total_reward())
                 game.close()
--- rl-algorithms/pg/pg_doom/pgnet.py
+++ rl-algorithms/pg/pg_doom/pgnet.py
@@ -9,9 +9,19 @@ class PGNetwork:
 
         with tf.variable_scope(name):
             with tf.name_scope("inputs"):
-                self.inputs_ = tf.placeholder(tf.float32, [None, *state_size], name="inputs_")
-                self.actions = tf.placeholder(tf.int32, [None, action_size], name="actions")
-                self.discounted_episode_rewards_ = tf.placeholder(tf.float32, [None, ], name="discounted_episode_rewards_")
+                self.inputs_ = tf.placeholder(
+                    tf.float32, [None, *state_size], name="inputs_"
+                )
+                self.actions = tf.placeholder(
+                    tf.int32, [None, action_size], name="actions"
+                )
+                self.discounted_episode_rewards_ = tf.placeholder(
+                    tf.float32,
+                    [
+                        None,
+                    ],
+                    name="discounted_episode_rewards_",
+                )
                 # Tensorboard
                 self.mean_reward_ = tf.placeholder(tf.float32, name="mean_reward")
 
@@ -19,53 +29,56 @@ class PGNetwork:
 
             with tf.name_scope("conv1"):
                 # Input is 84x84x4
-                self.conv1 = tf.layers.conv2d(inputs = self.inputs_,\
-                                            filters = 32, \
-                                            kernel_size = [8, 8],\
-                                            strides = [4, 4],\
-                                            padding = "VALID",\
-                                            kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(),\
-                                            name = "conv1")
+                self.conv1 = tf.layers.conv2d(
+                    inputs=self.inputs_,
+                    filters=32,
+                    kernel_size=[8, 8],
+                    strides=[4, 4],
+                    padding="VALID",
+                    kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                    name="conv1",
+                )
 
-                self.conv1_batchnorm = tf.layers.batch_normalization(self.conv1,\
-                                                                    training = True,\
-                                                                    epsilon = 1e-5,\
-                                                                    name = "batch_norm1")
+                self.conv1_batchnorm = tf.layers.batch_normalization(
+                    self.conv1, training=True, epsilon=1e-5, name="batch_norm1"
+                )
 
                 self.conv1_out = tf.nn.elu(self.conv1_batchnorm, name="conv1_out")
                 # ## [20, 20, 32]
 
             ########################################################################################################################
             with tf.name_scope("conv2"):
-                self.conv2 = tf.layers.conv2d(inputs = self.conv1_out, \
-                                            filters = 64,\
-                                            kernel_size = [4, 4], \
-                                            strides = [2, 2], \
-                                            padding = "VALID", \
-                                            kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(), \
-                                            name = "conv2")
+                self.conv2 = tf.layers.conv2d(
+                    inputs=self.conv1_out,
+                    filters=64,
+                    kernel_size=[4, 4],
+                    strides=[2, 2],
+                    padding="VALID",
+                    kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                    name="conv2",
+                )
 
-                self.conv2_batchnorm = tf.layers.batch_normalization(self.conv2,\
-                                                                    training = True,\
-                                                                    epsilon = 1e-5,\
-                                                                    name = "batch_norm2")
+                self.conv2_batchnorm = tf.layers.batch_normalization(
+                    self.conv2, training=True, epsilon=1e-5, name="batch_norm2"
+                )
 
                 self.conv2_out = tf.nn.elu(self.conv2_batchnorm, name="conv2_out")
 
             ########################################################################################################################
             with tf.name_scope("conv3"):
-                self.conv3 = tf.layers.conv2d(inputs = self.conv2_out, \
-                                            filters = 128,\
-                                            kernel_size = [4, 4], \
-                                            strides = [2, 2], \
-                                            padding = "VALID", \
-                                            kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d(), \
-                                            name = "conv3")
+                self.conv3 = tf.layers.conv2d(
+                    inputs=self.conv2_out,
+                    filters=128,
+                    kernel_size=[4, 4],
+                    strides=[2, 2],
+                    padding="VALID",
+                    kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                    name="conv3",
+                )
 
-                self.conv3_batchnorm = tf.layers.batch_normalization(self.conv3,\
-                                                                    training = True,\
-                                                                    epsilon = 1e-5,\
-                                                                    name = "batch_norm3")
+                self.conv3_batchnorm = tf.layers.batch_normalization(
+                    self.conv3, training=True, epsilon=1e-5, name="batch_norm3"
+                )
 
                 self.conv3_out = tf.nn.elu(self.conv3_batchnorm, name="conv3_out")
                 # --> [3, 3, 128]
@@ -75,18 +88,22 @@ class PGNetwork:
                 # -> [1152]
             ########################################################################################################################
             with tf.name_scope("fc"):
-                self.fc = tf.layers.dense(inputs = self.flatten,\
-                                        units = 512,\
-                                        activation = tf.nn.elu,\
-                                        kernel_initializer = tf.contrib.layers.xavier_initializer(),\
-                                        name = "fc")
+                self.fc = tf.layers.dense(
+                    inputs=self.flatten,
+                    units=512,
+                    activation=tf.nn.elu,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                    name="fc",
+                )
 
             ########################################################################################################################
             with tf.name_scope("logits"):
-                self.logits = tf.layers.dense(inputs = self.fc,\
-                                            kernel_initializer = tf.contrib.layers.xavier_initializer(),\
-                                            units = self.action_size,\
-                                            activation = None)
+                self.logits = tf.layers.dense(
+                    inputs=self.fc,
+                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                    units=self.action_size,
+                    activation=None,
+                )
 
             ########################################################################################################################
             with tf.name_scope("softmax"):
@@ -94,13 +111,19 @@ class PGNetwork:
 
             ########################################################################################################################
             with tf.name_scope("loss"):
-                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.actions)
-                self.loss = tf.reduce_mean(self.neg_log_prob * self.discounted_episode_rewards_)
-            
+                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(
+                    logits=self.logits, labels=self.actions
+                )
+                self.loss = tf.reduce_mean(
+                    self.neg_log_prob * self.discounted_episode_rewards_
+                )
+
             ########################################################################################################################
             with tf.name_scope("train"):
-                self.train_opt = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
-                                             
+                self.train_opt = tf.train.RMSPropOptimizer(self.learning_rate).minimize(
+                    self.loss
+                )
+
 
 if __name__ == "__main__":
     print("Testing for indent errors...")
